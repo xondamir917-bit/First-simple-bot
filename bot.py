2eef511d0a3e5923822756d9c93800d0b55d1ea6
xondamir917-bit <xondamir917@gmail.com>
2025-09-24 12:04:51 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,18 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+
+async def start(update: Update, context):
+    await update.message.reply_text("Good morning. How are you?\nMy commands:\n/start - New message\n/help - My self")
+
+async def help(update: Update, context):
+    await update.message.reply_text("My self. My name is Simple bot.")
+
+async def echo(update: Update, context):
+    await update.message.reply_text(update.message.text)
+
+app = Application.builder().token("7914991356:AAEruVaGjKLq3QI1tuxCNL-DnPj3LtHfRlo").build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("help", help))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+app.run_polling()
